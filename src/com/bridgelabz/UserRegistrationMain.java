e998cd869391de4303f84ab4dfb193d061bdc6be
rutuja-nandiwadekar <nandiwadekarrutuja@gmail.com>
2022-03-17 22:31:32 +0530

    Modified: Ability to check all email samples are valid or not logic

--- src/com/bridgelabz/UserRegistrationMain.java
+++ src/com/bridgelabz/UserRegistrationMain.java
@@ -20,35 +20,35 @@ public class UserRegistrationMain extends UserDetailsValidation {
 
 	// UC1 Ability to add first name
 	public static void userFirstName() {
-		System.out.println("Enter your First Name");
+		System.out.println("Enter your First Name:");
 		firstName = scanner.nextLine();
 		UserDetailsValidation.isValidFirstName(firstName);
 	}
 
 	// UC2 Ability to add last name
 	public static void userLastName() {
-		System.out.println("Enter your Last Name");
+		System.out.println("Enter your Last Name:");
 		lastName = scanner.nextLine();
 		UserDetailsValidation.isValidFirstName(lastName);
 	}
 
 	// UC3 Ability to add email
 	public static void userEmail() {
-		System.out.println("Enter your Email");
+		System.out.println("Enter your Email:");
 		email = scanner.nextLine();
 		UserDetailsValidation.isValidEmail(email);
 	}
 
 	// UC4 Ability to add mobile number
 	public static void userMobileNumber() {
-		System.out.println("Enter your mobile Number");
+		System.out.println("Enter your mobile Number:");
 		mobileNumber = scanner.nextLine();
 		UserDetailsValidation.isValidMobileNumber(mobileNumber);
 	}
 
-	// UC5-UC8 Ability to add Password
+	// UC5- UC8 Ability to add Password
 	public static void userPassword() {
-		System.out.println("Enter your password");
+		System.out.println("Enter your password:");
 		password = scanner.nextLine();
 		UserDetailsValidation.isValidPassword(password);
 	}
